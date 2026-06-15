1e66882f2688e3f6025e8af8b1c37c1e31fc5747
fivekilo <3060882350@qq.com>
2026-06-15 15:17:32 +0800

    上传报告模板

---
+++ .tmp_docx_dump.py
@@ -0,0 +1,11 @@
+﻿from zipfile import ZipFile
+from lxml import etree
+p=r'E:\project\数据挖掘第二组报告\第二组数据挖掘与信息检索实验报告.docx'
+ns={'w':'http://schemas.openxmlformats.org/wordprocessingml/2006/main'}
+with ZipFile(p) as z:
+    xml=z.read('word/document.xml')
+root=etree.fromstring(xml)
+for para in root.xpath('//w:p', namespaces=ns):
+    txt=''.join(para.xpath('.//w:t/text()', namespaces=ns)).strip()
+    if txt:
+        print(txt)
